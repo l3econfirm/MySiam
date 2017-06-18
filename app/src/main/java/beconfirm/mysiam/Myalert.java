5ed1d5f015976db2b052b9e30888f85ce4a87e05
beconfirm <l3econfirm@hotmail.com>
2017-06-18 15:16:29 +0700

    Alert Success

---
+++ app/src/main/java/beconfirm/mysiam/Myalert.java
@@ -0,0 +1,36 @@
+package beconfirm.mysiam;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by Admin on 18/6/2560.
+ */
+
+public class Myalert {
+
+    private Context context;
+
+    public Myalert(Context context) {
+        this.context = context;
+    }
+
+
+    public void myDialog(String strTitle, String strMessage) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.mipmap.ic_name);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+            }
+        });
+        builder.show();
+    }
+} //mainclass
--- app/src/main/java/beconfirm/mysiam/RegisterActivity.java
+++ app/src/main/java/beconfirm/mysiam/RegisterActivity.java
@@ -2,12 +2,81 @@ package beconfirm.mysiam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    //Explicit
+    private ImageView imageView;
+    private EditText nameEditText, userEditText, passwordEditText;
+    private Button button;
+
+    private String nameString, userString, passwordString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Initial View
+        initialView();
+
+        //Back Controller
+        backController();
+
+        //newRegister Controller
+        newRegisterController();
+
+    } //Main Method
+
+    private void newRegisterController() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Get value From edit text
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //check space
+                if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+                    //have space
+                    Myalert myalert = new Myalert(RegisterActivity.this);
+                    myalert.myDialog("Have space", "Please Fill");
+                } else {
+                    //no space
+
+                }
+
+
+
+            }
+        });
     }
-}
+
+    private void backController() {
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void initialView() {
+        imageView = (ImageView) findViewById(R.id.btnblack);
+        nameEditText= (EditText) findViewById(R.id.edtname);
+        userEditText= (EditText) findViewById(R.id.edtuser);
+        passwordEditText= (EditText) findViewById(R.id.edtpass);
+        button= (Button) findViewById(R.id.btnNewRegister);
+    }
+
+
+} //์Main Classs
