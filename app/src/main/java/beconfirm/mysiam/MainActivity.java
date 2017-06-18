ff735f3b3438265b19b672accb74a980e2b4a710
beconfirm <l3econfirm@hotmail.com>
2017-06-18 10:41:02 +0700

    Intent to Register

--- app/src/main/java/beconfirm/mysiam/MainActivity.java
+++ app/src/main/java/beconfirm/mysiam/MainActivity.java
@@ -1,13 +1,59 @@
 package beconfirm.mysiam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //explicits
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initial View
+        InitialView();
+
+        //Textview Controlled
+        textviewControlled();
+
+
+    } //method Main
+
+    private void textviewControlled() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //intent to newRegisterActivity
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
-}
+
+    private void InitialView(){
+
+        userEditText = (EditText) findViewById(R.id.editUser);
+        passwordEditText = (EditText) findViewById(R.id.editPass);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+
+
+
+    }
+
+} //main Class
